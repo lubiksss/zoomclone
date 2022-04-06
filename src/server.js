f719c57631a24dedd843ac79d4db0d50a81e4a12
jake.0208(조재덕)/kakao <jake.0208@kakaocorp.com>
2022-04-07 01:04:34 +0900

    FEAT: ws로 채팅방 기본 기능 구현

--- src/server.js
+++ src/server.js
@@ -15,13 +15,24 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`)
 const server = http.createServer(app);
 const wss = new WebSocketServer({server});
 
+const sockets = [];
+
 wss.on("connection", (socket) => {
+    sockets.push(socket)
+    socket["nickname"] = "Anon";
     console.log("Connected to Browser ✅")
     socket.on("close", () => console.log("Disconnected from the Browser ❌"))
     socket.on("message", message => {
-        console.log(message.toString('utf8'));
+        const messageObj = JSON.parse(message);
+        switch (messageObj.type) {
+            case "new_message":
+                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${messageObj.payload}`));
+                break
+            case "nickname":
+                socket["nickname"] = messageObj.payload;
+                break
+        }
     })
-    socket.send("hello!!");
 })
 
 server.listen(3000, handleListen);
