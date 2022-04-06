4e0ac6d3b79773f1dc0c7a06c80a5de590a2bb34
jake.0208(조재덕)/kakao <jake.0208@kakaocorp.com>
2022-04-06 21:50:49 +0900

    FEAT: websocket기초, browser, server 연결 확인

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import {WebSocketServer} from "ws";
 import express from "express";
 
 const app = express();
@@ -5,10 +7,21 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
-app.get("/", (req,res)=> res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+const handleListen = () => console.log(`Listening on http://localhost:3000`)
 
-console.log("hello");
+const server = http.createServer(app);
+const wss = new WebSocketServer({server});
 
-app.listen(3000);
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser ✅")
+    socket.on("close", () => console.log("Disconnected from the Browser ❌"))
+    socket.on("message", message => {
+        console.log(message.toString('utf8'));
+    })
+    socket.send("hello!!");
+})
+
+server.listen(3000, handleListen);
