199dd52db55b87ad20727c7822b5c0125523bc45
jake.0208(조재덕)/kakao <jake.0208@kakaocorp.com>
2022-04-09 17:00:04 +0900

    FEAT: 채팅방 완성
    재밌다.

--- src/server.js
+++ src/server.js
@@ -15,19 +15,41 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`)
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
+function publicRooms() {
+    const sids = wsServer.sockets.adapter.sids;
+    const rooms = wsServer.sockets.adapter.rooms;
+    const publicRooms = [];
+    rooms.forEach((_, key) => {
+        if (sids.get(key) === undefined) {
+            publicRooms.push(key);
+        }
+    })
+    return publicRooms
+}
+
+function countRoom(roomName) {
+    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+
 wsServer.on("connection", socket => {
     socket["nickname"] = "Anon"
     socket.onAny((event) => {
+        console.log(wsServer.sockets.adapter)
         console.log(`Socket event:${event}`);
     })
     socket.on("enter_room", (roomName, done) => {
         socket.join(roomName);
         done();
-        socket.to(roomName).emit("welcome", socket.nickname);
+        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName));
+        wsServer.sockets.emit("room_change", publicRooms())
     });
     socket.on("disconnecting", () => {
-        socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname))
+        socket.rooms.forEach((room) =>
+            socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1))
     })
+    socket.on("disconnect", () => {
+        wsServer.sockets.emit("room_change", publicRooms())
+    });
     socket.on("new_message", (msg, room, done) => {
         socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
         done();
